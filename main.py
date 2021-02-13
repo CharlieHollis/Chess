c0e3bc30edfe01767b4e3a2975e97694206f7e6c
Charlie Hollis <charlie.hollis1@btinternet.com>
2021-02-13 12:14:49 +0000

    main

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ pygame.init()
 height, width = 864,1000
 
 display = pygame.display.set_mode((width,height))
-black = (0,0,0)
+darkSquare = (118,150,86)
 white = (255,255,255)
-brown = (193,154,107)
+selected = (186,202,43)
 
 
 wood = pygame.image.load("wood.jpg")
@@ -31,7 +31,7 @@ while running:
         for j in range(8):
             if ((i+1)+(j+1)) % 2 == 0:
                 pygame.draw.rect(display, white, (100+i*100,46+j*100,100,100))
-            else: pygame.draw.rect(display, black, (100+i*100,46+j*100,100,100))
+            else: pygame.draw.rect(display, darkSquare, (100+i*100,46+j*100,100,100))
     pygame.display.update()
     clock.tick(60)
 
