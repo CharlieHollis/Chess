e4b7e7dd6cd09229e7f69450def96ffe2af1d9f5
Charlie Hollis <charlie.hollis1@btinternet.com>
2021-02-13 14:26:15 +0000

    main

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ import boardSection as bs
 
 pygame.init()
 
-height, width = 864,1000
+height, width = 1000,1000
 
 display = pygame.display.set_mode((width,height))
 darkSquare = (118,150,86)
 white = (255,255,255)
 selected = (186,202,43)
 
-board = []
+boardBg = []
 
 for i in range(8):
-    board.append([])
+    boardBg.append([])
     for j in range(8):
         if ((i+1)+(j+1)) % 2 == 0:
-            board[i].append(bs.board(white, 100+i*100, 46+j*100, i+j))
-        else: board[i].append(bs.board(darkSquare, 100+i*100, 46+j*100, i+j))
+            boardBg[i].append(bs.board(os.path.join("imgs","lightSquare.png"), 100+i*100, 100+j*100, i+j))
+        else: boardBg[i].append(bs.board(os.path.join("imgs","darkSquare.png"), 100+i*100, 100+j*100, i+j))
 
 
 wood = pygame.image.load(os.path.join("imgs","wood.jpg"))
@@ -33,15 +33,16 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEMOTION:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            
             # print(pygame.mouse.get_pos())
             pass
 
-    display.blit(wood, (0,0)); display.blit(wood, (894,0))      # Creates Background
+    display.blit(wood, (0,0)); display.blit(wood, (894,0)); display.blit(wood, (0,894)); display.blit(wood, (894,894))      # Creates Background
 
-    for i in range(len(board)):             # Creates Board
-        for j in range(len(board[i])):
-            pygame.draw.rect(display, board[i][j].colour, (board[i][j].posx,board[i][j].posy,board[i][j].sizex,board[i][j].sizey))
+    for i in range(len(boardBg)):             # Creates Board
+        for j in range(len(boardBg[i])):
+            display.blit(boardBg[i][j].colour, (boardBg[i][j].posx, boardBg[i][j].posy))
 
 
     display.blit(rook[0],(100,46))
